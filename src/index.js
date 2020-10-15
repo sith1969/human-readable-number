f073a13e87d75672476fc5e2d838f5cc4326e74c
sith1969 <sith@tut.by>
2020-10-15 11:15:12 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+'use strict';
 module.exports = function toReadable (number) {
-  
-}
+    let name1 = {0: '', 1 : 'one', 2: 'two', 3 : 'three', 4 : 'four', 5 : 'five', 
+    6 : 'six', 7 : 'seven', 8 : 'eight', 9 : 'nine', 10 : 'ten', 11 : 'eleven', 
+    12 : 'twelve', 13 : 'thirteen', 14 : 'fourteen', 15 : 'fifteen', 16 : 'sixteen',
+     17 : 'seventeen', 18 : 'eighteen', 19 : 'nineteen'};
+    let name2 = {2 : 'twenty', 3 : 'thirty', 4 : 'forty', 5 : 'fifty', 6 : 'sixty', 7 : 'seventy',
+     8 : 'eighty', 9 : 'ninety'};
+    let result = '';
+    if(number === 0) {return 'zero';}
+    if(number >= 100) {result += name1[Math.trunc(number / 100)] + ' hundred ';}
+    if(number % 100 < 20) {
+      result += name1[number % 100];  
+    } else {
+      result += name2[Math.trunc((number % 100) / 10)];
+      if (number % 10) {result += ' ' + name1[number % 10]; }
+    }
+      return result.trim();
+};
